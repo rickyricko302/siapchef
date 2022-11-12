39327744cced38731e081daac92b41efd6ed824f
rickyricko302 <rickyricko302@gmail.com>
2022-11-12 14:04:09 +0700

    Fix bug certificate http

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,7 +11,17 @@ import 'page/detail_resep/detail_resep_cubit.dart';
 import 'page/koleksi/koleksi_cubit.dart';
 import 'repository/db_repository.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() {
+  HttpOverrides.global = new MyHttpOverrides();
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
   runApp(const SiapChef());
 }
